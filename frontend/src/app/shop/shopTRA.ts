9a3b491277bb39ee6c8c87ab33f9045770591bde
Marcus Freire <marcusfreiremartins@gmail.com>
2023-12-09 16:34:31 -0300

    adicionando métodos na dashboard e shop

--- frontend/src/app/shop/shopTRA.ts
+++ frontend/src/app/shop/shopTRA.ts
@@ -1,29 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Product } from "../../../../backend/src/models/product"
+import { PurchaseProduct } from '../../../../backend/src/models/purchase-product';
 
 @Injectable({
-    providedIn: 'root',
-  })
-  export class ProductService {
-    private apiUrl = 'http://localhost:8000';
-  
-    constructor(private http: HttpClient) {}
-  
-    getProductsById(productParams: any): Observable<Product> {
-      return this.http.post<Product>(`${this.apiUrl}/api/product`, productParams);
-    }
+  providedIn: 'root',
+})
+export class PurchaseService {
+  private apiUrl = 'http://localhost:8000';
 
-    insertProduct(productParams: any): Observable<Product> {
-        return this.http.post<Product>(`${this.apiUrl}/api/product`, productParams);
-      }
+  constructor(private http: HttpClient) {}
 
-      updateProduct(productParams: any): Observable<Product> {
-        return this.http.post<Product>(`${this.apiUrl}/api/product`, productParams);
-      }
-
-      deleteProduct(productParams: any): Observable<Product> {
-        return this.http.post<Product>(`${this.apiUrl}/api/product`, productParams);
-      }      
+  getPurchasesByUserId(purchaseProductParam: any): Observable<PurchaseProduct> {
+    return this.http.post<PurchaseProduct>(
+      `${this.apiUrl}/api/`,
+      purchaseProductParam
+    );
   }
+
+  insertPurchase(purchaseProductParam: any): Observable<PurchaseProduct> {
+    return this.http.post<PurchaseProduct>(
+      `${this.apiUrl}/api/`,
+      purchaseProductParam
+    );
+  }
+}
