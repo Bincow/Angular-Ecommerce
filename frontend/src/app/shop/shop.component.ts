7f92da7960bfeb3a7782e611ebc59fa58e4dec35
jonataspm <jonataspmpm@gmail.com>
2023-12-10 14:36:38 -0300

    Feat: Tela do shop

--- frontend/src/app/shop/shop.component.ts
+++ frontend/src/app/shop/shop.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './shop.component.html',
-  styleUrl: './shop.component.css'
+  styleUrl: './shop.component.scss'
 })
 export class ShopComponent {
+
+  products:any[] = [
+    {
+      Id:'65716f234f73a2ad083c6ad9', 
+      name:  "Capinha",
+      price:  10,
+      quantity:  2,
+      types:  null,
+      image:  null
+    },
+    {
+      Id:'657171474f73a2ad083c6ada', 
+      name:  "Capinha 2",
+      price:  10,
+      quantity:  30,
+      types:  [0,1],
+      image:  null
+    }
+  ] 
+
 
 }
